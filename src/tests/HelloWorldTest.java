fb1727acecaa91f2813b6d7a3af9273602f7d8fe
user <taras.kor@gmail.com>
2021-10-03 15:51:56 -0500

    updated Hello World Test Class

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -5,5 +5,6 @@ public class HelloWorldTest {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("One more Hello World");
     }
 }
