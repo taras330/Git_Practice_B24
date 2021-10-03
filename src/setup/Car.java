8fa4711b2e62b408cba0dc84ac0a873c9319f486
user <taras.kor@gmail.com>
2021-10-03 15:19:56 -0500

    updated car in master branch

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,18 +2,18 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     // this is car class from master branch
 
     // we will have conflict
 
     // because there is different text in different branches for car
-=======
+
     // this our car from james branch
 
     // more text
 
     // more text
->>>>>>> james
+
 
 }
