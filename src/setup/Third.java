3591b4ec2be39b8b262ebb26d2e7a151bb189ac8
user <taras.kor@gmail.com>
2021-10-03 11:43:25 -0500

    Added third file

---
+++ src/setup/Third.java
@@ -0,0 +1,11 @@
+package setup;
+
+public class Third {
+
+    public static void main(String[] args) {
+
+        System.out.println("add some text");
+
+        System.out.println("this is text sample for third file");
+    }
+}
