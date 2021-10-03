ff035f8b85269f5e5575060f819e552df5d4ae8b
user <taras.kor@gmail.com>
2021-10-03 11:44:02 -0500

    Updated third file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -7,5 +7,7 @@ public class Third {
         System.out.println("add some text");
 
         System.out.println("this is text sample for third file");
+
+        // planning to add new text in 30 minutes
     }
 }
