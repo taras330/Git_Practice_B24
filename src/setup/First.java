3eb6cb1710115058adbd65be8508168e1083aa64
user <taras.kor@gmail.com>
2021-10-03 10:31:27 -0500

    Updating first file

--- src/setup/First.java
+++ src/setup/First.java
@@ -5,5 +5,11 @@ public class First {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        // This is a new line
+
+        // I want to create a history
+
+        // The text does not matter
     }
 }
